af3d92d12bccbdd444440887f609178bfce5139d
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-06-26 22:13:33 -0500

    Update separateOddsAndEvens.test.js

--- week-3/separateOddsAndEvens.test.js
+++ week-3/separateOddsAndEvens.test.js
@@ -1,7 +1,19 @@
 const { separateOddsAndEvens } = require("./separateOddsAndEvens");
 
 describe("separateOddsAndEvens", () => {
-  it("should separate odd and even numbers into 2 arrays", () => {
+  test("WHEN array is [1,2] THEN it returns [[1], [2]]", () => {
+    expect(separateOddsAndEvens([1, 2])).toEqual([[1], [2]]);
+  });
+
+  test("WHEN array is [10,20,30] THEN it returns [[], [10,20,30]]", () => {
+    expect(separateOddsAndEvens([10, 20, 30])).toEqual([[], [10, 20, 30]]);
+  });
+
+  test("WHEN array is [11,21,31] THEN it returns [[11,21,31], []]", () => {
+    expect(separateOddsAndEvens([11, 21, 31])).toEqual([[11, 21, 31], []]);
+  });
+
+  it("should separate random odd and even numbers into 2 arrays", () => {
     const input = [];
 
     for (let i = 0; i < 100; i++) {
@@ -9,13 +21,7 @@ describe("separateOddsAndEvens", () => {
     }
 
     const output = separateOddsAndEvens(input);
-
-    for (const num of output[0]) {
-      expect(num % 2).toBe(1);
-    }
-
-    for (const num of output[1]) {
-      expect(num % 2).toBe(0);
-    }
+    expect(output[0].every((num) => num % 2 === 1)).toBe(true);
+    expect(output[1].every((num) => num % 2 === 0)).toBe(true);
   });
 });
